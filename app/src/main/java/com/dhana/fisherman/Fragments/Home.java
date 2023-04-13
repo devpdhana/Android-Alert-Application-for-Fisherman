003066913b1aef9cd7c1f4bcc85018fbcf34025e
devpdhana <devpdhanasekar@gmail.com>
2023-04-13 09:47:47 +0530

    Fisherman application

--- app/src/main/java/com/dhana/fisherman/Fragments/Home.java
+++ app/src/main/java/com/dhana/fisherman/Fragments/Home.java
@@ -1,22 +1,142 @@
 package com.dhana.fisherman.Fragments;
 
+import static android.widget.Toast.*;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.dhana.fisherman.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class Home extends Fragment {
+import java.util.Properties;
 
-    @Nullable
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class Home extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private TextView locationTextView;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.home_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+
+        // Initialize the location client
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        // Get the location TextView
+        locationTextView = view.findViewById(R.id.txtLocat);
+
+        // Initialize the map fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.gMap);
+        mapFragment.getMapAsync(this);
+
         return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Check if location permission is granted
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+
+        // Get the user's location
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            // Update the location TextView with the new location
+                            double latitude = location.getLatitude();
+                            double longitude = location.getLongitude();
+                            locationTextView.setText("Latitude: " + latitude + "\nLongitude: " + longitude);
+
+                            if (latitude > 12.7987478){
+                                final MediaPlayer mediaPlayer = MediaPlayer.create(getContext(), R.raw.siren);
+                                mediaPlayer.setLooping(false);
+                                final Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mediaPlayer.start();
+                                        handler.postDelayed(this,120000);
+                                    }
+                                },0);
+
+//                                // Send SMS
+//                                String phoneNumber = "8525907295"; // Replace with the phone number to send the message to
+//                                String message = "Latitude: " + latitude + ", Longitude: " + longitude; // Replace with your message
+//                                SmsManager smsManager = SmsManager.getDefault();
+//                                smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+
+                            }
+
+                            // Move the camera to the new location
+                            LatLng latLng = new LatLng(latitude, longitude);
+                            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+                        }
+                    }
+                });
+    }
+
+    // Handle location permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+                }
+            }
+        }
+    }
 }
+
